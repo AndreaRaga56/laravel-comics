0fd4050335a114db4200e28436e8a0bc55d067fc
AndreaRaga56 <andrearaga56@gmail.com>
2025-04-23 23:26:16 +0200

    main footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -44,7 +44,54 @@
 
     <div class="main-footer">
         <div class="container">
-            <div>provaaaaaaaaaaaaaaaaaaaaaaa</div>
+            <div class="row row-cols-3">
+                <div class="col">
+                    <h2>DC COMICS</h2>
+                    <ul>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Comics</a></li>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">TV</a></li>
+                        <li><a href="#">Games</a></li>
+                        <li><a href="#">Video</a></li>
+                        <li><a href="#">News</a></li>
+                    </ul>
+
+                    <h2>SHOP</h2>
+                    <ul>
+                        <li><a href="#">Shop DC</a></li>
+                        <li><a href="#">Shop DC Collectibles</a></li>
+                    </ul>
+                </div>
+
+                <div class="col">
+                    <h2>DC</h2>
+                    <ul>
+                        <li><a href="#">Terms Of Use</a></li>
+                        <li><a href="#">Privacy Policy</a></li>
+                        <li><a href="#">Ad Choices</a></li>
+                        <li><a href="#">Advertising</a></li>
+                        <li><a href="#">Jobs</a></li>
+                        <li><a href="#">Subscriptions</a></li>
+                        <li><a href="#">Talent Workshops</a></li>
+                        <li><a href="#">CPSC Certificates</a></li>
+                        <li><a href="#">Ratings</a></li>
+                        <li><a href="#">Shop Help</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
+                </div>
+
+                <div class="col">
+                    <h2>SITES</h2>
+                    <ul>
+                        <li><a href="#">DC</a></li>
+                        <li><a href="#">MAD Magazine</a></li>
+                        <li><a href="#">DC Kids</a></li>
+                        <li><a href="#">DC Universe</a></li>
+                        <li><a href="#">DC Power Visa</a></li>
+                    </ul>
+                </div>
+            </div>
             <img src="{{ Vite::asset('resources/img/dc-logo-bg.png') }}" alt="">
         </div>
     </div>
