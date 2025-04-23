37778aba83951ea8562eb94b739445e826d00edc
AndreaRaga56 <andrearaga56@gmail.com>
2025-04-23 06:00:32 +0200

    top footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,51 @@
+<div class="top-footer">
+    <div class="container">
+        <div class="row row-col-5">
 
+            <div class="col">
+                <div class="cta">
+                    <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="">
+                    <p>DIGITAL COMICS</p>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="cta">
+                    <img src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}" alt="">
+                    <p>DC MERCHANDISE</p>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="cta">
+                    <img src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}" alt="">
+                    <p>SUBSCRIPTION</p>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="cta">
+                    <img src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}" alt="">
+                    <p>COMIC SHOP LOCATOR</p>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="cta">
+                    <img src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg') }}" alt="">
+                    <p>DC POWER VISA</p>
+                </div>
+            </div>
+
+
+
+
+
+
+
+
+
+        </div>
+    </div>
+
+</div>
